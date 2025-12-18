402d0e1b1f398492b10ea08d22c1bcd796c1233e
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-12-18 20:03:33 +0500

    Add app.py model.ipynb & model.pkl

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pickle
+
+st.title("Salary Predictor")
+st.set_page_config(page_title="Salary Predictor", page_icon="💵")
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+yoe = st.number_input("Enter the Years of Experience", min_value=0.0, max_value=11.0, step=0.5, value=1.0)
+if st.button("Predict"):
+    predicted_salary = model.predict([[yoe]])
+    st.success(predicted_salary)
